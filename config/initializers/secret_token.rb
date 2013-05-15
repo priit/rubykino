afece7f781e033d4afe98d8560ea2a7155852729
Priit Tamboom <priit@gitlab.eu>
2013-05-15 19:28:57 +0300

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubykino::Application.config.secret_token = '6099f6fc759390f2d15b79638477fa0608b20edef57e6b29543b3a32f08d0923e74f4c35aa9660acb264340819759d89c7c887b8670ba3e58c12783ee6a358b6'
