ddff915c785e47fddff19df6f3a7df324d07f4bd
Priit Tamboom <priit@gitlab.eu>
2014-08-29 13:12:07 +0300

    updated date logic

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,35 +1,39 @@
 class Meetup # no table yet
   class << self
     def next_date
-      meetup_dates.sort {|a,b| a <=> b}.first
+      dates.sort {|a,b| a <=> b}.first
     end
 
-    def meetup_dates
-      [feb, may, aug, oct]
+    def dates
+      [feb, may, aug, nov]
     end
 
     def feb
-      (Chronic.parse('thursday of next March') - 1.week).to_date
+      feb = Chronic.parse('last thursday', :now => Chronic.parse('february').end_of_month).to_date
+      return feb if feb > Date.today
+      Chronic.parse('last thursday', :now => Chronic.parse('february').end_of_month + 1.year).to_date
     end
 
     def may
-      (Chronic.parse('thursday of next June') - 1.week).to_date
+      may = Chronic.parse('last thursday', :now => Chronic.parse('may').end_of_month).to_date
+      return may if may > Date.today
+      Chronic.parse('last thursday', :now => Chronic.parse('may').end_of_month + 1.year).to_date
     end
 
     def aug
-      (Chronic.parse('thursday of next September') - 1.week).to_date
+      aug = Chronic.parse('last thursday', :now => Chronic.parse('august').end_of_month).to_date
+      return aug if aug > Date.today
+      Chronic.parse('last thursday', :now => Chronic.parse('august').end_of_month + 1.year).to_date
     end
 
-    def oct
-      (Chronic.parse('thursday of next November') - 1.week).to_date
+    def nov
+      nov = Chronic.parse('last thursday', :now => Chronic.parse('november').end_of_month).to_date
+      return nov if nov > Date.today
+      Chronic.parse('last thursday', :now => Chronic.parse('november').end_of_month + 1.year).to_date
     end
 
     def last_date
-      last_feb = (Chronic.parse('thursday of last March') - 1.week).to_date
-      last_may = (Chronic.parse('thursday of last June') - 1.week).to_date
-      last_aug = (Chronic.parse('thursday of last September') - 1.week).to_date
-      last_oct = (Chronic.parse('thursday of last November') - 1.week).to_date
-      [last_feb, last_may, last_aug, last_oct].sort {|a,b| a <=> b}.last
+      Chronic.parse('last thursday', :now => (Meetup.next_date - 3.months).end_of_month).to_date
     end
   end
 end
